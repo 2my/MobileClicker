a56f1cace9aa277967524974cb2542cf76dc688e
Tommy Skodje <tsk@antares.no>
2012-08-31 15:20:08 +0200

    First cut interface

--- android/src/no/antares/mobile/clicker/Main.java
+++ android/src/no/antares/mobile/clicker/Main.java
@@ -1,20 +1,60 @@
+/* Main.java
+   Copyright 2012 Tommy Skodje
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package no.antares.mobile.clicker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * @author tommy skodje
+*/
 public class Main extends Activity {
+	private static final String TAG	= Main.class.getSimpleName();
+	private TextView notes	= null;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        notes = (TextView) findViewById( R.id.text_notes );
+        attachStepButtonListener( R.id.button_next, "+1" );
+        attachStepButtonListener( R.id.button_previous, "-1" );
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    private void attachStepButtonListener( int buttonId, final String text ) {
+        Button next = (Button) findViewById( buttonId );
+        next.setOnClickListener(
+    		new OnClickListener() {
+		    	public void onClick(View v) {
+					Log.d( TAG, text + " button clicked!");
+					notes.setText( text );
+		    	}
+	    	}
+        );
+    };
+
 }
