232802e6b980614cd1ca0489fa314b01ae900b8a
Tommy Skodje <tsk@antares.no>
2012-09-06 22:33:39 +0200

    Fixed indentation

--- server/src/main/java/no/antares/mobile/clicker/QRCode.java
+++ server/src/main/java/no/antares/mobile/clicker/QRCode.java
@@ -1,3 +1,4 @@
+/* from http://stackoverflow.com/questions/2489048/qr-code-encoding-and-decoding-using-zxing */
 package no.antares.mobile.clicker;
 
 import java.io.File;
@@ -16,17 +17,17 @@ import com.google.zxing.common.BitMatrix;
 
 /* Generates QR-code for a given string
 @see http://stackoverflow.com/questions/2489048/qr-code-encoding-and-decoding-using-zxing
-*/
+ */
 public class QRCode {
-    final static String charSetName	= "UTF-8";
+	final static String charSetName	= "UTF-8";
 	// public static final String fileName = "/Users/tommyskodje/Downloads/TSK_QR_S.png";
 	public static final String fileName = "QR.png";
 	// TODO: use streams, not file
 
 	final int h;
-    final int w;
+	final int w;
 
-    /**  */
+	/**  */
 	public QRCode( int height, int width ) {
 		this.h = height;
 		this.w = width;
@@ -34,30 +35,30 @@ public class QRCode {
 
 	public void generate( String text ) {
 		byte[] b = text.getBytes( Charset.forName( charSetName ) );
-	
-	    try {
-	    	String data = new String(b, charSetName );
-	        // get a byte matrix for the data
-	        BitMatrix matrix = null;
-	        Writer writer = new MultiFormatWriter();
-	        try {
-	            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>(2);
-	            hints.put(EncodeHintType.CHARACTER_SET, charSetName);
-	            matrix = writer.encode( data, BarcodeFormat.QR_CODE, w, h, hints );
-	        } catch ( WriterException e ) {
-	            System.out.println(e.getMessage());
-	        }
-	
-	        try {
-		        File file = new File( fileName );
-	            MatrixToImageWriter.writeToFile(matrix, "PNG", file);
-	            System.out.println("printing to " + file.getAbsolutePath());
-	        } catch (IOException e) {
-	            System.out.println(e.getMessage());
-	        }
-	    } catch (UnsupportedEncodingException e) {
-	        System.out.println(e.getMessage());
-	    }
+
+		try {
+			String data = new String(b, charSetName );
+			// get a byte matrix for the data
+			BitMatrix matrix = null;
+			Writer writer = new MultiFormatWriter();
+			try {
+				Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>(2);
+				hints.put(EncodeHintType.CHARACTER_SET, charSetName);
+				matrix = writer.encode( data, BarcodeFormat.QR_CODE, w, h, hints );
+			} catch ( WriterException e ) {
+				System.out.println(e.getMessage());
+			}
+
+			try {
+				File file = new File( fileName );
+				MatrixToImageWriter.writeToFile(matrix, "PNG", file);
+				System.out.println("printing to " + file.getAbsolutePath());
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		} catch (UnsupportedEncodingException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
